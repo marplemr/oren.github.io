856132c262b4266cae31d53d96940d0f84ab58a7
Oren <orengolan@gmail.com>
2015-05-17 22:33:22 -0700

    icons

---
+++ javascript/main.js
@@ -0,0 +1,22 @@
+// console.log('This would be the main JS file.');
+// 
+// var github = document.querySelector('.github');
+// var twitter = document.querySelector('.twitter');
+// var linkedin = document.querySelector('.linkedin');
+// 
+// github.addEventListener('mouseover', over);
+// github.addEventListener('mouseout', out);
+// twitter.addEventListener('mouseover', over);
+// twitter.addEventListener('mouseout', out);
+// linkedin.addEventListener('mouseover', over);
+// linkedin.addEventListener('mouseout', out);
+// 
+// function over() {
+//   var site = this.classList[0]
+//   this.src='file:///home/oren/projects/oren.github.io/images/' + site + '_icon_pink.png'
+// }
+// 
+// function out() {
+//   var site = this.classList[0]
+//   this.src='file:///home/oren/projects/oren.github.io/images/' + site + '_icon.png'
+// }
