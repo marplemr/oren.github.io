af29b6dfb7a222317da1decf28eaaa2194329714
Oren <orengolan@gmail.com>
2015-05-24 21:50:30 -0700

    basic nav

--- javascript/main.js
+++ javascript/main.js
@@ -1,29 +1,29 @@
-function ready(fn) {
-  if (document.readyState != 'loading') {
-    fn();
-  } else {
-    document.addEventListener('DOMContentLoaded', fn);
-  }
-}
-
-ready(run);
-
-function run() {
-  var blogLink = document.querySelector('.blog-link');
-  var videosLink = document.querySelector('.videos-link');
-  var blog = document.querySelector('.blog');
-  var videos = document.querySelector('.videos');
-
-  blogLink.addEventListener('click', showBlog);
-  videosLink.addEventListener('click', showVideos);
-
-  function showBlog() {
-    blog.style.display = 'block';
-    videos.style.display = 'none';
-  }
-
-  function showVideos() {
-    videos.style.display = 'block';
-    blog.style.display = 'none';
-  }
-}
+// function ready(fn) {
+//   if (document.readyState != 'loading') {
+//     fn();
+//   } else {
+//     document.addEventListener('DOMContentLoaded', fn);
+//   }
+// }
+// 
+// ready(run);
+// 
+// function run() {
+//   var blogLink = document.querySelector('.blog-link');
+//   var videosLink = document.querySelector('.videos-link');
+//   var blog = document.querySelector('.blog');
+//   var videos = document.querySelector('.videos');
+// 
+//   blogLink.addEventListener('click', showBlog);
+//   videosLink.addEventListener('click', showVideos);
+// 
+//   function showBlog() {
+//     blog.style.display = 'block';
+//     videos.style.display = 'none';
+//   }
+// 
+//   function showVideos() {
+//     videos.style.display = 'block';
+//     blog.style.display = 'none';
+//   }
+// }
