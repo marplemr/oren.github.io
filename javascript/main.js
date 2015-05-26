abf3deb5c4b930e9d8b0a7ea79f68b75d2d8a209
Oren <orengolan@gmail.com>
2015-05-25 18:47:28 -0700

    auto-hide nav

--- javascript/main.js
+++ javascript/main.js
@@ -27,3 +27,21 @@
 //     blog.style.display = 'none';
 //   }
 // }
+//
+
+var previousScroll = 0;
+var headerOrgOffset = $('#header').height();
+
+$('#header-wrap').height($('#header').height());
+
+$(window).scroll(function () {
+  var currentScroll = $(this).scrollTop();
+  if (currentScroll > headerOrgOffset) {
+    if (currentScroll > previousScroll) {
+      $('#header-wrap').slideUp();
+    } else {
+      $('#header-wrap').slideDown();
+    }
+  } 
+  previousScroll = currentScroll;
+});
